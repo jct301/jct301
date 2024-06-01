1fe2d38de7bf2575ec28fab1051970e1a4c3b19f
jctr301 <jctr301@gmail.com>
2024-05-31 21:10:13 -0400

    add github action

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const WEBSITE = 'https://jeancarlos-dev.vercel.app/'
+
+export const NUMBER_OF = {
+    ARTICLES: 5,
+    PROJECTS: 5,
+  }
+  
+  export const PLACEHOLDERS = {
+    LATEST_ARTICLES: '%{{latest_articles}}%',
+    LATEST_PROJECTS: '%{{latest_projects}}%',
+  }
