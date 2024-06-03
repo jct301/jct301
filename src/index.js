e6128491ef509324c12e9e64d1bd0a10332edc57
jctr301 <jctr301@gmail.com>
2024-06-02 21:51:23 -0400

    correction code

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
 import { promises as fs } from 'fs'
 import Parser from 'rss-parser'
-import { WEBSITE,NUMBER_OF,PLACEHOLDERS } from './constants.js'
+import { NUMBER_OF, PLACEHOLDERS, WEBSITE } from './constants.js'
 
 const parser = new Parser()
 
-const getLatestArticlesFromBlog = () =>
-  parser.parseURL(`${WEBSITE}/rss.xml`).then((data) => data.items.filter(({link}) => link.split('/')[3]=='blog'))
-
-const getLatestProjects = () =>
-  parser.parseURL(`${WEBSITE}/rss.xml`).then((data) => data.items.filter(({link}) => link.split('/')[3]=='projects'))
-
-
+const getLatestArticlesFromBlog = () => {
+  return parser.parseURL(`${WEBSITE}/rss.xml`).then((data) => data.items.filter(({ link }) => link.split('/')[3] === 'blog'
+  ))
+}
+const getLatestProjects = () => {
+  return parser.parseURL(`${WEBSITE}/rss.xml`).then((data) => data.items.filter(({ link }) => link.split('/')[3] === 'projects'))
+}
 
 ;(async () => {
-  
-    const [template, articles, projects] = await Promise.all([
-      fs.readFile('./src/README.md.tpl', { encoding: 'utf-8' }),
-      getLatestArticlesFromBlog(),
-      getLatestProjects()
-    ])
-  
-    // create latest articles markdown
-    const latestArticlesMarkdown = articles
-      .slice(0, NUMBER_OF.ARTICLES)
-      .map(({ title, link }) => `- [${title}](${link})`)
-      .join('\n')
-  
-          // create latest articles markdown
-    const latestProjectsMarkdown = projects
+  const [template, articles, projects] = await Promise.all([
+    fs.readFile('./src/README.md.tpl', { encoding: 'utf-8' }),
+    getLatestArticlesFromBlog(),
+    getLatestProjects()
+  ])
+
+  // create latest articles markdown
+  const latestArticlesMarkdown = articles
+    .slice(0, NUMBER_OF.ARTICLES)
+    .map(({ title, link }) => `- [${title}](${link})`)
+    .join('\n')
+
+  // create latest articles markdown
+  const latestProjectsMarkdown = projects
     .slice(0, NUMBER_OF.PROJECTS)
     .map(({ title, link }) => `- [${title}](${link})`)
     .join('\n')
-   
-    // replace all placeholders with info
-    const newMarkdown = template
+
+  // replace all placeholders with info
+  const newMarkdown = template
     .replace(PLACEHOLDERS.LATEST_ARTICLES, latestArticlesMarkdown)
     .replace(PLACEHOLDERS.LATEST_PROJECTS, latestProjectsMarkdown)
 
-    await fs.writeFile('README.md', newMarkdown)
-  })()
+  await fs.writeFile('README.md', newMarkdown)
+})()
